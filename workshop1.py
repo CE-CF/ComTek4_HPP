7b152c6b2e9473b7a17f3917d1e8ec430833b817
c3lphie <c3lphie@protonmail.com>
2022-05-02 08:38:23 +0200

    update

--- workshop1.py
+++ workshop1.py
@@ -1,15 +1,16 @@
 import numpy as np
 import time
 
+
 def kadane(arr, start, finish, n):
- 
+
     Sum = 0
     maxSum = -999999999999
     i = None
- 
-    finish[0] = -1 
+
+    finish[0] = -1
     local_start = 0
- 
+
     for i in range(n):
         Sum += arr[i]
         if Sum < 0:
@@ -19,40 +20,39 @@ def kadane(arr, start, finish, n):
             maxSum = Sum
             start[0] = local_start
             finish[0] = i
- 
+
     if finish[0] != -1:
         return maxSum
- 
+
     maxSum = arr[0]
     start[0] = finish[0] = 0
- 
+
     for i in range(1, n):
         if arr[i] > maxSum:
             maxSum = arr[i]
             start[0] = finish[0] = i
     return maxSum
-  
 
-def findMaxSum(M):
-    global ROW, COL
- 
+
+def findMaxSum(M, ROW, COL):
+
     maxSum, finalLeft = -999999999999, None
     finalRight, finalTop, finalBottom = None, None, None
     left, right, i = None, None, None
- 
+
     temp = [None] * ROW
     Sum = 0
     start = [0]
     finish = [0]
- 
+
     for left in range(COL):
- 
+
         temp = [0] * ROW
- 
+
         for right in range(left, COL):
             for i in range(ROW):
                 temp[i] += M[i][right]
- 
+
             Sum = kadane(temp, start, finish, ROW)
 
             if Sum > maxSum:
@@ -63,59 +63,55 @@ def findMaxSum(M):
                 finalBottom = finish[0]
 
     return (maxSum, finalTop, finalLeft, finalBottom, finalRight)
- 
 
 
- 
+def matrix_Generator(N, out=True):
+    print("The dimension : " + str(N))  # printing dimension
 
-def matrix_Generator(N):      
-    print("The dimension : " + str(N))     # printing dimension
-      
-    res = [list(np.random.randint(-1,2,N)) for i in range(N)] # using list comprehension - matrix creation of n * n
-      
-    print("The created matrix of N * N: \n")     # print result
-    column = ''
-    divider = ''
-    for x in range(N):
-        if x == 0:
-            column = '       {:4d}'.format(x)
-            divider = '     ------'
-        else:
-            column = column+'{:4d}'.format(x)
-            divider = divider+'----'
-    print(column)
-    print(divider)
+    res = [
+        list(np.random.randint(-1, 2, N)) for i in range(N)
+    ]  # using list comprehension - matrix creation of n * n
 
-    for x in range(len(res)):
-        row = ''
-        for y in range(len(res[x])):
-            if y == 0:
-                row = row+'{:4d} | {:4d}'.format(x, res[x][y])
+    if out:
+        print("The created matrix of N * N: \n")  # print result
+        column = ""
+        divider = ""
+        for x in range(N):
+            if x == 0:
+                column = "       {:4d}".format(x)
+                divider = "     ------"
             else:
-                row = row+'{:4d}'.format(res[x][y])
-        print(row)
-    print("")
+                column = column + "{:4d}".format(x)
+                divider = divider + "----"
+        print(column)
+        print(divider)
+
+        for x in range(len(res)):
+            row = ""
+            for y in range(len(res[x])):
+                if y == 0:
+                    row = row + "{:4d} | {:4d}".format(x, res[x][y])
+                else:
+                    row = row + "{:4d}".format(res[x][y])
+            print(row)
+        print("")
     return res
 
 
-
 # Driver Code
-if __name__=='__main__': 
+if __name__ == "__main__":
 
-    N = 15          # N * N matrix
+    N = 10 ** 4  # N * N matrix
     ROW = N
-    COL = N    
-    M = matrix_Generator(N)
+    COL = N
+    M = matrix_Generator(N, False)
 
     t_start = time.time()
-    maxSum, finalTop, finalLeft, finalBottom, finalRight   = findMaxSum(M)
+    maxSum, finalTop, finalLeft, finalBottom, finalRight = findMaxSum(M)
     t_end = time.time()
 
-    print("(Top, Left)", "(", finalTop,
-          finalLeft, ")")
-    print("(Bottom, Right)", "(", finalBottom,
-          finalRight, ")")
+    print("(Top, Left)", "(", finalTop, finalLeft, ")")
+    print("(Bottom, Right)", "(", finalBottom, finalRight, ")")
     print("Max sum is:", maxSum)
 
-    print("Time for sequential execution: ",t_end-t_start)
- 
+    print("Time for sequential execution: ", t_end - t_start)
