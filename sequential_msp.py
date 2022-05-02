eba91c7486c3b34abb50ea588582a9488bcefa74
c3lphie <c3lphie@protonmail.com>
2022-05-02 08:37:02 +0200

    Rename of sequential msp script

--- sequential_msp.py
+++ sequential_msp.py
@@ -1,15 +1,16 @@
 import numpy as np
 import time
 
+
 def kadane(arr, start, finish, n):
- 
+
     Sum = 0
     maxSum = -999999999999
     i = None
- 
-    finish[0] = -1 
+
+    finish[0] = -1
     local_start = 0
- 
+
     for i in range(n):
         Sum += arr[i]
         if Sum < 0:
@@ -19,40 +20,40 @@ def kadane(arr, start, finish, n):
             maxSum = Sum
             start[0] = local_start
             finish[0] = i
- 
+
     if finish[0] != -1:
         return maxSum
- 
+
     maxSum = arr[0]
     start[0] = finish[0] = 0
- 
+
     for i in range(1, n):
         if arr[i] > maxSum:
             maxSum = arr[i]
             start[0] = finish[0] = i
     return maxSum
-  
+
 
 def findMaxSum(M):
     global ROW, COL
- 
+
     maxSum, finalLeft = -999999999999, None
     finalRight, finalTop, finalBottom = None, None, None
     left, right, i = None, None, None
- 
+
     temp = [None] * ROW
     Sum = 0
     start = [0]
     finish = [0]
- 
+
     for left in range(COL):
- 
+
         temp = [0] * ROW
- 
+
         for right in range(left, COL):
             for i in range(ROW):
                 temp[i] += M[i][right]
- 
+
             Sum = kadane(temp, start, finish, ROW)
 
             if Sum > maxSum:
@@ -63,59 +64,57 @@ def findMaxSum(M):
                 finalBottom = finish[0]
 
     return (maxSum, finalTop, finalLeft, finalBottom, finalRight)
- 
 
 
- 
+def matrix_Generator(N):
+    # print("The dimension : " + str(N))     # printing dimension
 
-def matrix_Generator(N):      
-    #print("The dimension : " + str(N))     # printing dimension
-      
-    res = [list(np.random.randint(-1,2,N)) for i in range(N)] # using list comprehension - matrix creation of n * n
-      
-    #print("The created matrix of N * N: \n")     # print result
-    column = ''
-    divider = ''
+    res = [
+        list(np.random.randint(-1, 2, N)) for i in range(N)
+    ]  # using list comprehension - matrix creation of n * n
+
+    # print("The created matrix of N * N: \n")     # print result
+    column = ""
+    divider = ""
     for x in range(N):
         if x == 0:
-            column = '       {:4d}'.format(x)
-            divider = '     ------'
+            column = "       {:4d}".format(x)
+            divider = "     ------"
         else:
-            column = column+'{:4d}'.format(x)
-            divider = divider+'----'
-    #print(column)
-    #print(divider)
+            column = column + "{:4d}".format(x)
+            divider = divider + "----"
+    # print(column)
+    # print(divider)
 
     for x in range(len(res)):
-        row = ''
+        row = ""
         for y in range(len(res[x])):
             if y == 0:
-                row = row+'{:4d} | {:4d}'.format(x, res[x][y])
+                row = row + "{:4d} | {:4d}".format(x, res[x][y])
             else:
-                row = row+'{:4d}'.format(res[x][y])
-        #print(row)
-    #print("")
+                row = row + "{:4d}".format(res[x][y])
+        # print(row)
+    # print("")
     return res
 
 
-
 # Driver Code
-if __name__=='__main__': 
+if __name__ == "__main__":
 
-    N = 1000          # N * N matrix
+    N = 1000  # N * N matrix
     ROW = N
-    COL = N    
+    COL = N
     M = matrix_Generator(N)
 
     time_arr = 0
 
     for x in range(10):
         t_start = time.time()
-        maxSum, finalTop, finalLeft, finalBottom, finalRight   = findMaxSum(M)
+        maxSum, finalTop, finalLeft, finalBottom, finalRight = findMaxSum(M)
         t_end = time.time()
-        print(t_end-t_start)
-        time_arr += (t_end-t_start)
-    print("Average time for 10 runs:",time_arr/10)
+        print(t_end - t_start)
+        time_arr += t_end - t_start
+    print("Average time for 10 runs:", time_arr / 10)
 
 """
     print("(Top, Left)", "(", finalTop,
